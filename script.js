d3bce5a305940670f60addf2c55ea5f6c5702663
Hamdan-sid <hamdansiddiqui10@icloud.com>
2024-11-20 02:05:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let popup = "Error! Please enter a valid password."
+alert(popup) 
+let welcome = "Welcome to JS land \n Happy Coding!" 
+alert(welcome)
+let happy = "Happy Coding \n Prevent this page from creating additional pages."
+alert(happy)
+let hello ="Hello! I can run JS through my browser console."
+alert(hello)
+let username = "Jhone Doe"
+alert(username)
+let age = "15 years old"
+alert(age)
+let certify = "Certified Mobile Application Development"
+alert(certify)
+let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+let email = "My email address is hamdan@icloud.com"
+alert(email)
+let learn = "I am trying to learn from the book.A smarter way to learn JS."
+alert(learn)
+let times ="You visited this site 14 times."
+alert(times)
+let order =" John doe oredred 5 t-sirts on this store."
+alert(order)
+let shirt ="Blue Hoodie"
+alert(shirt)
+let price = "1200 PKR"
+alert(price)
+let greet = "Thank You For Shopping"
+alert(greet)
